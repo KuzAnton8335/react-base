8b688d1b44365ac14134eaa40bed11e32be508a8
KuzAnton8335 <ant2783@gmail.com>
2025-01-12 20:53:51 +0300

    delete tik-tak-toe

--- module-3 compose-components/tik-tak-toe/src/GameLayout/GameLayout.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './game.module.css'; // CSS-модуль для стилей
-
-export const GameLayout = ({ children }) => {
-	return <div className={styles.GameLayout}>{children}</div>;
-};
--- module-3 compose-components/tik-tak-toe/src/Information/InformationLayout.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './information.module.css'; // CSS-модуль для стилей
-
-const InformationLayout = ({ children }) => {
-	return <div className={styles.InformationLayout}>{children}</div>;
-};
-
-export default InformationLayout;
